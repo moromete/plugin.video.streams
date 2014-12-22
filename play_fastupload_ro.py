ab2195f463eac0e5d34d4642954e40cf2a5d1bba
Moromete <you@example.com>
2014-12-22 23:05:28 +0200

    commit

--- play_fastupload_ro.py
+++ play_fastupload_ro.py
@@ -5,12 +5,22 @@ from glob import addon_log, Downloader, message, addon
 from settings import SETTINGS
 
 def grab_fu_stream(name, url):
-  temp = os.path.join(SETTINGS.ADDON_PATH,"temp.htm")
-  Downloader(url, temp, addon.getLocalizedString(30065), name)  #Downloading page for parsing stream url
-  f = open(temp)
-  source_txt = f.read()
-  f.close()
-  os.remove(temp)
+  
+  
+  try:
+    opener = urllib2.build_opener()
+    opener.addheaders = [('User-agent', 'Mozilla/5.0')]
+    response = opener.open(url)
+    source_txt = response.read()
+  except Exception as inst:
+    source_txt = ""
+  
+  #temp = os.path.join(SETTINGS.ADDON_PATH,"temp.htm")
+  #Downloader(url, temp, addon.getLocalizedString(30065), name)  #Downloading page for parsing stream url
+  #f = open(temp)
+  #source_txt = f.read()
+  #f.close()
+  #os.remove(temp)
 
   #addon_log(url)
   #addon_log(source_txt)
